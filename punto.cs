6f8315d2383e8c26a1ba44a43d7c9a392755dd89
Kristan Ruíz <itan_07@hotmail.com>
2023-02-15 08:17:32 -0800

    v1 Creacion Recta Alpha

---
+++ punto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forms
+{
+    internal class Punto
+    {
+        private float x;
+        private float y;
+
+        public Punto()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Punto(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ recta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forms
+{
+    internal class recta
+    {
+        private Punto a;
+        private Punto b;
+
+        public recta()
+        {
+            a = new Punto();
+            b = new Punto();
+        }
+        public recta(Punto a, Punto b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
